96caa30c826fd40d0fe0c0fd3d48ae3011d84d00
cerendn <cerendncer@outlook.com>
2024-04-09 20:50:31 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Header from "./header";
 
-function App() {
+function PokemonApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="buton">
+        <input type="text" placeholder="Find Pokémon..." />
+        <input type="text" placeholder="Find Type..." />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default PokemonApp;
---
+++ src/header.js
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <h2>Pokémon App</h2>
+      </div>
+      <div className="option">
+        <a href="#">Pokémon Api</a>
+        <a href="#">Pokémon Oficial</a>
+        <a href="#">About Author</a>
+      </div>
+    </header>
+  );
+}
+export default Header;
