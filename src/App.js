6830982e81efeb17ee08f24ffa1da393b52a7de2
cerendn <cerendncer@outlook.com>
2024-04-09 21:33:48 +0300

    add states

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Header from "./Header";
 function PokemonApp() {
   //pokemonList state
   const [pokemonList, setPokemonList] = useState([]);
+  //searchPokemon state
+  const [searchPokemon, setSearchPokemon] = useState("");
+  //searchType state
+  const [searchType, setSearchType] = useState("");
 
   return (
     <div className="container">
