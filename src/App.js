142aeedbaeaff734f9709b111a607f21e8be6a0b
cerendn <cerendncer@outlook.com>
2024-04-11 23:07:51 +0300

    continue

--- src/App.js
+++ src/App.js
@@ -55,10 +55,12 @@ function PokemonApp() {
           onChange={(e) => filterPokemonByType(e.target.value)}
         />
       </div>
-      <div className="pokemon-container">
-        {pokemonList.map((pokemon, index) => (
-          <PokemonCard key={index} pokemonName={pokemon.name} />
-        ))}
+      <div className="pokemon">
+        <div className="pokemon-container">
+          {pokemonList.map((pokemon, index) => (
+            <PokemonCard key={index} pokemonName={pokemon.name} />
+          ))}
+        </div>
       </div>
       <footer></footer>
     </div>
