fcabbf9e6c2d1dc3e837e890c17dcd6d34a5e7f7
cerendn <cerendncer@outlook.com>
2024-04-11 22:48:47 +0300

    fetchAPI -pokemonCard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
+import PokemonCard from "./PokemonCard"; // PokemonCard bileşenini import et
 
 function PokemonApp() {
   //pokemonList state
@@ -10,13 +11,54 @@ function PokemonApp() {
   //searchType state
   const [searchType, setSearchType] = useState("");
 
+  //useEffect kullanarak API'den fetch ile veri çekme
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=150"
+        );
+        const data = await response.json();
+        setPokemonList(data.results);
+      } catch (error) {
+        console.error("Veriler alınırken hata oluştu:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  // Pokemon isimleri için filtreleme function
+  const filterPokemonByName = (name) => {
+    setSearchPokemon(name.toLowerCase());
+  };
+  // Types filtreleme function
+  const filterPokemonByType = (type) => {
+    setSearchType(type.toLowerCase());
+  };
+
   return (
     <div className="container">
       <Header />
       {/* search buton */}
       <div className="buton">
-        <input type="text" placeholder="Find Pokémon..." />
-        <input type="text" placeholder="Find Type..." />
+        <input
+          type="text"
+          placeholder="Find Pokémon..."
+          value={searchPokemon}
+          //isim için filter func çağır
+          onChange={(e) => filterPokemonByName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Find Type..."
+          value={searchType}
+          //types filter function çağır
+          onChange={(e) => filterPokemonByType(e.target.value)}
+        />
+      </div>
+      <div className="pokemon-container">
+        {pokemonList.map((pokemon, index) => (
+          <PokemonCard key={index} pokemonName={pokemon.name} />
+        ))}
       </div>
     </div>
   );
---
+++ src/PokemonCard.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+//pokeomon adına göre API'den verileri alıp gösterme
+const PokemonCard = ({ pokemonName }) => {
+  //verileri tutmak için state oluşturma
+  const [pokemonData, setPokemonData] = useState(null);
+
+  //useEffect hook ile her render edildiginde yada prop değiştiğinde çalıştırma
+  useEffect(
+    () => {
+      //asyn func ile API'den fetch ile veri çekme
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+          );
+          const data = await response.json();
+          setPokemonData(data);
+        } catch (error) {
+          console.error("Pokemon verisi alınırken hata oluştu:");
+        }
+      };
+
+      fetchData();
+    },
+    //sadece prop değişirse çalıştır
+    [pokemonName]
+  );
+  //pokemonData null ise kartı göstermez
+  if (!pokemonData) return null;
+
+  return (
+    //PokemonCardı gösterme
+    <div className="pokemon-card">
+      <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+      <h3>{`${pokemonData.id} - ${pokemonData.name}`}</h3>
+      <div className="pokemon-types">
+        <h4>Types:</h4>
+        {pokemonData.types.map((type, index) => (
+          <ul key={index} className="type-name">
+            {type.type.name}
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PokemonCard;
